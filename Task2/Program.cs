7d1a8fa42cb7b5d89ecd36c842e1f3f1eec19e20
Radmir Shagiev <veselday@gmail.com>
2023-04-21 13:44:41 +0500

    Third task

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,11 +1,11 @@
 ﻿// 2. Написать программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел
 
-Console.WriteLine("Please write number 1");
+    Console.WriteLine("Please write number 1");
 
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please write number 2");
+    Console.WriteLine("Please write number 2");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please write number 3");
+    Console.WriteLine("Please write number 3");
 int c = Convert.ToInt32(Console.ReadLine());
 
 int max = a;
---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+    Console.WriteLine("Please write number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a % 2 == 0)
+
+System.Console.WriteLine("Number " + a + " is even");
+
+    else System.Console.WriteLine("Number " + a + " is odd");
