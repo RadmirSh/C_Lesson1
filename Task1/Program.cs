0f97be3f4618edb06c4433d6098b977d333a47aa
Radmir Shagiev <veselday@gmail.com>
2023-04-21 12:48:42 +0500

    First task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Написать программу, которая на вход принимает 2 числа и выдает, какое из них больше, а какое меньше
+Console.WriteLine("Please write number 1: ");
+            int num_a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please write number 2: ");
+            int num_b = Convert.ToInt32(Console.ReadLine());
+
+            if (num_a > num_b)
+            {
+                Console.WriteLine("max value " + num_a);
+            }
+           if (num_a == num_b)
+            {
+                Console.WriteLine("values are equal ");
+            }
+            else
+            {
+                Console.WriteLine("max value " + num_b);
+            }
